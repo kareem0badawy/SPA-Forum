116b8f2dd66341bcb9b9487fb95b9ccde8fa13b1
kareembadawy94 <kareembadawy@trukto.com>
2019-08-14 00:03:57 +0200

    Like and Dislike system for reply

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ use Illuminate\Http\Request;
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
-// Route::post('/like/{reply}', 'LikeController@likeIt');
-// Route::delete('/like/{reply}', 'LikeController@unLikeIt');
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
 // Route::post('notifications', 'NotificationController@index');
 // Route::post('markAsRead', 'NotificationController@markAsRead');
 
