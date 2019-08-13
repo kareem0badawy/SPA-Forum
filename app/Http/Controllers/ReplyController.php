2a2f74eb01407b482d4bb5f8effd76939dd76314
kareembadawy94 <kareembadawy@trukto.com>
2019-08-13 22:53:11 +0200

    Finished CRUD Reply

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -1,85 +1,52 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Model\Reply;
 use Illuminate\Http\Request;
+use App\Model\Question;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\ReplyResource;
+use App\Notifications\NewReplyNotification;
+use App\Events\DeleteReplyEvent;
 
 class ReplyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+  
+    public function __construct()
     {
-        //
+        // $this->middleware('JWT', ['except' => ['index','show']]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+  
+    public function index(Question $question)
     {
-        //
+        return ReplyResource::collection($question->replies);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+ 
+    public function store(Question $question, Request $request)
     {
-        //
+        $reply = $question->replies()->create($request->all());
+        // $user = $question->user;
+        // if ($reply->user_id !== $question->user_id) {
+        //     $user->notify(new NewReplyNotification($reply));
+        // }
+        
+        return response(['reply' => new ReplyResource($reply)], Response::HTTP_CREATED);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Reply $reply)
+  
+    public function show(Question $question, Reply $reply)
     {
-        //
+        return new ReplyResource($reply);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Reply $reply)
+    
+    public function update(Question $question, Request $request, Reply $reply)
     {
-        //
+        $reply->update($request->all());
+        return response('Update', Response::HTTP_ACCEPTED);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Reply $reply)
+    
+    public function destroy(Question $question, Reply $reply)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Reply $reply)
-    {
-        //
+        $reply->delete();
+        // broadcast(new DeleteReplyEvent($reply->id))->toOthers();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
-// Route::apiResource('/question/{question}/reply', 'ReplyController');
+Route::apiResource('/question/{question}/reply', 'ReplyController');
 // Route::post('/like/{reply}', 'LikeController@likeIt');
 // Route::delete('/like/{reply}', 'LikeController@unLikeIt');
 // Route::post('notifications', 'NotificationController@index');
